0ac55707e521e4a3ed6726da9b3edd6d4c805af2
auprao <gdwotek@gmail.com>
2024-11-07 23:37:39 +0100

    grid and axis span improved

--- python/plt_wave_graph.py
+++ python/plt_wave_graph.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import PillowWriter
 from mpl_toolkits import mplot3d
 
+span = 4
+min_span, max_span  = -span, span
+
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
@@ -11,18 +14,18 @@ def sinus(x, y, t) :
 
 writer = PillowWriter(fps=10)
 
-xlist, ylist = np.meshgrid(np.linspace(-5, 5), np.linspace(-5, 5))
+xlist, ylist = np.meshgrid(np.linspace(min_span, max_span, 201), np.linspace(min_span, max_span, 201))
 zlist = []
 
 
 
-with writer.saving(fig, "sin_wave.gif", 200) :
+with writer.saving(fig, ".\python\sin_wave.gif", 200) :
     for t in np.linspace(0, 10, 51) :
         zlist = sinus(xlist, ylist, t)
 
         ax.contour(xlist, ylist, zlist, zdir='z', offset=-100, cmap='coolwarm')
 
-        ax.set(xlim=(-5, 5), ylim = (-5, 5), zlim=(-5, 5), xlabel="x", ylabel="y", zlabel="z")
+        ax.set(xlim=(min_span, max_span), ylim = (min_span, max_span), zlim=(-5, 5), xlabel="x", ylabel="y", zlabel="z")
 
         #ax.scatter(xlist, ylist, zlist)
         ax.plot_surface(xlist, ylist, zlist, cmap="Blues_r")
