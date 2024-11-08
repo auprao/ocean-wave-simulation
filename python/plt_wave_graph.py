4a2e21e8c9bff5c06ba497e69a81520607bbb03e
auprao <gdwotek@gmail.com>
2024-11-08 22:53:07 +0100

    using working euler formula, organized gifs

--- python/plt_wave_graph.py
+++ python/plt_wave_graph.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import PillowWriter
 from mpl_toolkits import mplot3d
 
+a = 0.5 # amplitude
 af = 0.3 # angular frequency (time)
-k = 0.8 # wave number (stretch)
-span = 4 # axes limits (graph)
+k = 0.5 # wave number (stretch)
+span = 10 # axes limits (graph)
 min_span, max_span  = -span, span
 
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
-def sinus(x, y, t) :
-    return np.sin(k*x + k*y + af*t) 
+def sin_wave(x, y, t) :
+    return a * np.sin(k*x + k*y + af*t) 
 
-def wave2(x, y, t) :
-    return np.sin(k*x + k*y + af*t) * np.sin(k*x + af*t) * np.sin(k*y + af*t) 
+def gerstner_wave(x, y, t) : 
+    return a * np.exp(complex(0,1)*(k*x + k*y + af*t))  
 
 writer = PillowWriter(fps=10)
 
@@ -23,9 +24,9 @@ xlist, ylist = np.meshgrid(np.linspace(min_span, max_span, 201), np.linspace(min
 zlist = []
 
 
-with writer.saving(fig, ".\python\sin_wave.gif", 200) :
+with writer.saving(fig, ".\python\gerstner_wave.gif", 200) :
     for t in np.linspace(0, 10, 51) :
-        zlist = sinus(xlist, ylist, t)
+        zlist = gerstner_wave(xlist, ylist, t)
 
         ax.contour(xlist, ylist, zlist, zdir='z', offset=-100, cmap='coolwarm')
 
