0443eaf8adfd0e3b7ab50e0b636c2fa91ae11d5b
auprao <gdwotek@gmail.com>
2024-11-09 01:03:59 +0100

    gerstner waves, quality gifs, sigma sum of waves

--- python/plt_wave_graph.py
+++ python/plt_wave_graph.py
@@ -1,11 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import PillowWriter
-from mpl_toolkits import mplot3d
 
+wave_count = 6
 a = 0.2 # amplitude
-af = 0.3 # angular frequency (time)
-k = 0.5 # wave number (stretch)
+af = 0.2 # angular frequency (time)
+kx = [] # wave number (stretch) for every wave
+ky = [] 
+for i in range(1, wave_count + 1) :
+    kx.append(np.random.rand())
+    ky.append(np.random.rand())
+    
 span = 10 # axes limits (graph)
 min_span, max_span  = -span, span
 
@@ -13,28 +18,28 @@ fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
 def sin_wave(x, y, t) :
-    return a * np.sin(k*x + k*y + af*t) 
+    return a * np.sin(kx[0]*x + ky[0]*y + af*t) 
 
 def gerstner_wave(x, y, t) : 
-    return sum(a * np.exp(complex(0,1)*(k*x + k*y + n*af*t + n)).real for n in range(1,7))
+    return sum(a * np.exp(complex(0,1)*(kx[n-1]*x + ky[n-1]*y + np.log(n+1)*af*t + n)).real for n in range(1, wave_count + 1))
 
 
-writer = PillowWriter(fps=10)
+writer = PillowWriter(fps=30)
 
-xlist, ylist = np.meshgrid(np.linspace(min_span, max_span, 256), np.linspace(min_span, max_span, 256))
+xlist, ylist = np.meshgrid(np.linspace(min_span, max_span, 512), np.linspace(min_span, max_span, 512))
 zlist = []
 
 
-with writer.saving(fig, ".\python\gerstner_wave.gif", 200) :
-    for t in np.linspace(0, 10, 51) :
+with writer.saving(fig, ".\python\gerstner_wave_4_1.gif", 256) :
+    for t in np.linspace(0, 40, 301) :
         zlist = gerstner_wave(xlist, ylist, t)
 
         ax.contour(xlist, ylist, zlist, zdir='z', offset=-100, cmap='coolwarm')
 
-        ax.set(xlim=(min_span, max_span), ylim = (min_span, max_span), zlim=(-5, 5), xlabel="x", ylabel="y", zlabel="z")
+        ax.set(xlim=(min_span, max_span), ylim = (min_span, max_span), zlim=(min_span, max_span), xlabel="x", ylabel="y", zlabel="z")
 
-        #ax.plot_surface(xlist, ylist, zlist, cmap="Blues_r")
-        ax.plot_surface(xlist, ylist, zlist)
+        ax.plot_surface(xlist, ylist, zlist, cmap="Blues_r")
+        #ax.plot_surface(xlist, ylist, zlist)
 
         writer.grab_frame()
 
